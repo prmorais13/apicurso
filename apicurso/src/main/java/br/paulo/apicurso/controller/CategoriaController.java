773db136c0f92eea3c2b56d0452a5716fc3d5d3a
prmorais13 <prmorais_13@hotmail.com>
2017-07-20 16:34:53 -0300

    Segundo commit - trabalho

--- apicurso/src/main/java/br/paulo/apicurso/controller/CategoriaController.java
+++ apicurso/src/main/java/br/paulo/apicurso/controller/CategoriaController.java
@@ -1,5 +1,30 @@
 package br.paulo.apicurso.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.paulo.apicurso.model.Categoria;
+import br.paulo.apicurso.repository.Categorias;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+	
+	@Autowired
+	private Categorias categorias;
+	
+	@GetMapping()
+	public List<Categoria> listar() {
+		return this.categorias.findAll();
+	}
+	
+	/*public ResponseEntity<?> listar(){
+		List<Categoria> categorias = this.categorias.findAll();
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
+	}*/
 
 }
