b68f630bc402d1cbb6b2cbfc53a9528fa1c8f53b
prmorais13 <prmorais_13@hotmail.com>
2017-07-23 00:03:20 -0300

    Terceiro commit - casa

--- apicurso/src/main/java/br/paulo/apicurso/controller/CategoriaController.java
+++ apicurso/src/main/java/br/paulo/apicurso/controller/CategoriaController.java
@@ -1,11 +1,21 @@
 package br.paulo.apicurso.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.paulo.apicurso.model.Categoria;
 import br.paulo.apicurso.repository.Categorias;
@@ -27,4 +37,20 @@ public class CategoriaController {
 		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
 	}*/
 
+	@PostMapping()
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva =  this.categorias.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPorCodigo(@PathVariable Long codigo) {
+		Categoria categoriaEncontrada = this.categorias.findOne(codigo);
+		return categoriaEncontrada == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(categoriaEncontrada);
+	}
 }
+
