8481a85fbe5a2262cd6c57f2b8bae123570b0bad
prmorais13 <prmorais_13@hotmail.com>
2017-07-28 00:18:44 -0300

    Nono commit 2 - Casa

--- apicurso/src/main/java/br/paulo/apicurso/repository/Lancamentos.java
+++ apicurso/src/main/java/br/paulo/apicurso/repository/Lancamentos.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.paulo.apicurso.model.Lancamento;
 import br.paulo.apicurso.repository.lancamento.LancamentosQuery;
 
+
 public interface Lancamentos extends JpaRepository<Lancamento, Long>, LancamentosQuery {
 	
 	//public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
