d5b86deab46819e2a32117bdd64bf61a6391efc1
prmorais13 <prmorais_13@hotmail.com>
2017-08-07 18:58:19 -0300

    Décimo commit - Trabalho

--- apicurso/src/main/resources/db/migration/V03__Cria_e_Registra_Lancamento.sql
+++ apicurso/src/main/resources/db/migration/V03__Cria_e_Registra_Lancamento.sql
@@ -15,4 +15,4 @@ CREATE TABLE lancamento (
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Compra revestimento', '2017-07-30', null, 108.00, 'Revestimento da calçada', 'DESPESA', 4, 1);
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Mão de obra', '2017-07-30', null, 450.00, 'Pagamento revestimento muro e calçada', 'DESPESA', 4, 1);
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Supermercado', '2017-07-10', '2017-07-30', 150.00, 'Três pacotes de leite', 'DESPESA', 2, 2);
-INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Compra sapato', '2017-08-30', null, 170.50, 'Sapato para festa', 'DESPESA', 1, 2);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Compra sapato', '2017-08-30', null, 170.50, 'Sapato para festa', 'DESPESA', 1, 7);
